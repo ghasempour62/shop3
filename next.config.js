9c8c16e631abbda39b7ec0830827ad0f97f11184
ghasempour62 <145551494+ghasempour62@users.noreply.github.com>
2023-11-24 13:36:45 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const { hostname } = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`);
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   images: {
     domains: [`${hostname}`]
   },
